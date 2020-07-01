8030df7554bfe7bb3b2dadb28821490ea2dd3641
lisbethgl <karolesmes@hotmail.com>
2020-07-01 02:04:35 -0500

    Docs add

---
+++ docs/precache-manifest.704c7b67a8733f7365db9441d94e7418.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35bb7836ffaafe5c416bdad44db73ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920afa666afaec449c17",
+    "url": "/static/css/main.16d70860.chunk.css"
+  },
+  {
+    "revision": "ddfc370ede98d9e59777",
+    "url": "/static/js/2.75f7596a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75f7596a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920afa666afaec449c17",
+    "url": "/static/js/main.5874edd4.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
